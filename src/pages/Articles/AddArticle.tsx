92d0a256c63ccdae9441ff372bb4b810f7ddbcf0
baseerx <bsronnet@gmail.com>
2025-11-20 12:59:56 +0500

    useEffect removed

--- src/pages/Articles/AddArticle.tsx
+++ src/pages/Articles/AddArticle.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import MainCard from "../../components/cards/MainCard";
 import Input from "../../components/form/input/InputField";
 import Select from "../../components/form/Select";
