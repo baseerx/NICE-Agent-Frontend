821b6e7afad50b41cf001c0c8cb8b76052ad4057
baseerx <bsronnet@gmail.com>
2025-12-22 12:40:27 +0500

    sign in page animated video added

--- src/components/common/GridShape.tsx
+++ src/components/common/GridShape.tsx
@@ -1,13 +1,15 @@
 export default function GridShape() {
-  return (
-    <>
-      <div className="fixed top-0 right-0 z-[-1] h-screen w-1/2">
-        <img
-          src="/images/img/PakistanPowerSector.png"
-          alt="grid"
-          className="h-full w-full object-cover"
-        />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="fixed top-0 right-0 z-[-1] h-screen w-1/2">
+                <video
+                    src="/images/img/PakistanPowerSector.mp4"
+                    autoPlay
+                    muted
+                    loop
+                    className="h-full w-full object-cover"
+                />
+            </div>
+        </>
+    );
 }
