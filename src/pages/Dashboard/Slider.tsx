4a4bac1f915cdb27ce0fd9f864de3630192b4a84
Muhammad Baseer Khan <baxeerkhan@gmail.com>
2026-02-19 12:10:30 +0500

    button made smaller in slider

--- src/pages/Dashboard/Slider.tsx
+++ src/pages/Dashboard/Slider.tsx
@@ -105,7 +105,7 @@ const Slider = () => {
               <div className="mb-3">
                 <button
                   onClick={() => navigate(-1)}
-                  className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg"
+                  className="px-2 py-1 bg-blue-600 hover:bg-blue-700 text-sm text-white rounded-lg"
                 >
                   Go Back
                 </button>
