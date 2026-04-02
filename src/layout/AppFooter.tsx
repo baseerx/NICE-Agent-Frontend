ffa62ed5e5ece5ffed4920f56023df8ce4535f77
Muhammad Baseer Khan <baxeerkhan@gmail.com>
2026-04-02 10:32:13 +0500

    footer text updated

--- src/layout/AppFooter.tsx
+++ src/layout/AppFooter.tsx
@@ -7,7 +7,7 @@ const AppFooter: React.FC = () => {
         <footer className="w-full border-t dark:bg-gray-900 dark:border-gray-800 bg-white">
             <div className="mx-auto max-w-7xl px-4 py-1 sm:px-6 lg:px-8">
                 <div className="flex text-center items-center justify-center text-sm text-gray-600 dark:text-gray-300 leading-none">
-                    © {year} — Designed &amp; Developed by Muhammad Baseer Khan, AD IT, ISMO. Powered by Agentic AI framework Google ADK.
+                    © {year} — NICE, Conceptualized by Zeeshan Khan (Dir Comms), Developed by Muhammad Baseer Khan (AD IT)
                 </div>
             </div>
         </footer>
